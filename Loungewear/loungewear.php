5bb9ef93057bf4ef5222002c6848aa5bfbecc144
dmatt7779 <dmatt7779@gmail.com>
2020-09-07 13:42:36 -0500

    Productos generales Loungewear

---
+++ Loungewear/loungewear.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOUNGEWEAR KLMA' HUMANS</title>
+<!-- CSS only -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+  integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link rel="stylesheet" href="./style.css">
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+  integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+  crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+  integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+  crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+  integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+  crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg">
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="circulo collapse navbar-collapse col-lg-1 col-md-1 col-sm-1" id="navbarNav">
+                <div class="nav-item circulo">
+                  <a class="nav-link" href="#"></a>
+                </div>
+              </div>
+              <div class="nav-item titulo col-lg-7 col-md-7">
+                <a class="nav-link" href="#"></a>
+              </div>
+              <div class="col nav-item col-lg-3 col-md-3 col">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item menu-puntos">
+                    <a class="nav-link" href="#"></a>
+                  </li>
+                  <li class="nav-item menu-log">
+                    <a class="nav-link" href="#"></a>
+                  </li>
+                  <li class="nav-item menu-candado">
+                    <a class="nav-link" href="#"></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          </div>
+        </nav>
+    </header>
+
+<!-- Espacio Horizontal para imagenes de punta a punta, mantener las margenes de las imagenes horizontales -->
+
+<!--     <div class="gridhorizontal">
+        <div>
+            <a href="#"><img src="img/Sin título-3.jpg" alt=""></a>
+            <div class="card">
+            <a href="#" class="card-title"><p>T-SHIRT BRAND</p></a>
+            <p class="card-text">CAMISETA MANGA SISA BORDADA</p>
+            <a href="#" class="price-horizontal"><p>$270.000</p></a>
+            </div>
+        </div>
+    </div> -->
+
+<!-- Rejilla Vertical alineada al centro -->
+    <div class="gridvertical">
+        <div class="card-body">
+            <a href="#"><img src="img/primera.jpg" alt=""></a>
+            <a href="#" class="card-title"><p>CALM DRESS SOUL DRESS</p></a>
+            <p class="card-text">CHOMPA MANGA CORTA CON SESGOS EN CONTRASTE</p>
+            <a href="#" class="price">$270.000</a>
+        </div>
+    </div>
+
+<!-- Grid horizontal Importante mantener margenes de las imagenes en esta sección -->
+    <div class="gridhorizontal">
+        <div>
+            <a href="#"><img src="img/Sin título-3.jpg" alt=""></a>
+            <div class="card">
+            <a href="#" class="card-title"><p>T-SHIRT BRAND</p></a>
+            <p class="card-text">CAMISETA MANGA SISA BORDADA</p>
+            <a href="#" class="price-horizontal"><p>$270.000</p></a>
+            </div>
+        </div>
+    </div>
+
+<!-- Rejilla Vertical alineada al centro -->
+    <div class="gridvertical">
+        <div class="card-body">
+            <a href="#"><img src="img/tercera.jpg" alt=""></a>
+            <a href="#" class="card-title"><p>KNITTED SWEATER</p></a>
+            <p class="card-text">BUSO TEJIDO CON MANGAS ABOMBADAS</p>
+            <a href="#" class="price">$270.000</a>
+        </div>
+    </div>
+
+<!-- Grid de las imagenes verticales a ambos lados contenidas dentro de una misma rejilla, pero su espacio esta repartido en ambas -->
+    <div class="griddoblevertical">
+        <div class="card-body">
+            <div><a href="#"><img src="img/wengue.jpg" alt=""></a></div>
+            <a href="#" class="card-title"><p>COMFORTABLE BLAZER</p></a>
+            <p class="card-text">BLAZER CON RAYAS ACOLCHADAS</p>
+            <a href="#" class="price">$370.000</a>
+        </div>
+
+        <div class="card-body">
+            <div><a href="#"><img src="img/wengue2.jpg" alt=""></a></div>
+            <a href="#" class="card-title"><p>BIG JACKET</p></a>
+            <p class="card-text">CHAQUETA OVERSIZE AFELPADA</p>
+            <a href="#" class="price">$370.000</a>
+        </div>
+    </div>
+
+<!-- Grid horizontal Importante mantener margenes de las imagenes en esta sección -->
+    <div class="gridhorizontal">
+        <div>
+            <a href="#"><img src="img/Sin título-3.jpg" alt=""></a>
+            <div class="card">
+            <a href="#" class="card-title"><p>T-SHIRT BRAND</p></a>
+            <p class="card-text">CAMISETA MANGA SISA BORDADA</p>
+            <a href="#" class="price-horizontal"><p>$270.000</p></a>
+            </div>
+        </div>
+    </div>
+
+<!-- Rejilla Vertical alineada al centro -->
+    <div class="gridvertical">
+        <div class="card-body">
+            <a href="#"><img src="img/tercera.jpg" alt=""></a>
+            <a href="#" class="card-title"><p>KNITTED SWEATER</p></a>
+            <p class="card-text">BUSO TEJIDO CON MANGAS ABOMBADAS</p>
+            <a href="#" class="price">$270.000</a>
+        </div>
+    </div>
+
+<!-- Grid de las imagenes verticales a ambos lados contenidas dentro de una misma rejilla, pero su espacio esta repartido en ambas -->
+    <div class="griddoblevertical">
+        <div class="card-body">
+            <div><a href="#"><img src="img/wengue.jpg" alt=""></a></div>
+            <a href="#" class="card-title"><p>COMFORTABLE BLAZER</p></a>
+            <p class="card-text">BLAZER CON RAYAS ACOLCHADAS</p>
+            <a href="#" class="price">$370.000</a>
+        </div>
+
+        <div class="card-body">
+            <div><a href="#"><img src="img/wengue2.jpg" alt=""></a></div>
+            <a href="#" class="card-title"><p>BIG JACKET</p></a>
+            <p class="card-text">CHAQUETA OVERSIZE AFELPADA</p>
+            <a href="#" class="price">$370.000</a>
+        </div>
+    </div>
+
+<!-- Grid horizontal Importante mantener margenes de las imagenes en esta sección -->
+    <div class="gridhorizontal">
+        <div>
+            <a href="#"><img src="img/Sin título-3.jpg" alt=""></a>
+            <div class="card">
+            <a href="#" class="card-title"><p>T-SHIRT BRAND</p></a>
+            <p class="card-text">CAMISETA MANGA SISA BORDADA</p>
+            <a href="#" class="price-horizontal"><p>$270.000</p></a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
