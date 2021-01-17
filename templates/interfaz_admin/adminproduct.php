7331860dab85cd5c1acf39eae1c9103421be4faf
jerson <jecheverri69@misena.edu.co>
2021-01-17 17:59:26 -0500

    admin tabla producto

--- templates/interfaz_admin/adminproduct.php
+++ templates/interfaz_admin/adminproduct.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include "../../global/conexion.php";
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,11 +9,28 @@ include "../../global/conexion.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PRODUCTOS KLMA' HUMANS</title>
+    
 
     <!-- CSS only -->
     <link rel="stylesheet" href="../assets/librerias/bootstrap.min.css">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link rel="stylesheet" href="../assets/style/style.css">
+    <link rel="stylesheet" href="../assets/librerias/datatables.min.css">
+
+
+
+     
+        <script src="../assets/js/jquery.min.js"></script>
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="../assets/js/popper.min.js"></script>
+        <script src="../assets/js/bootstrap.min.js"></script>
+
+        <!-- Datatables -->
+        <link rel="stylesheet" href="../assets/style/datatables.min.css">
+        <script src="../assets/js/datatables.min.js"></script>   
+        
+     
 </head>
 <body>
 <?php include "../navbar_footer/header.php";?>
@@ -40,7 +58,77 @@ include "../../global/conexion.php";
         <!-- DIV PARA EL DATATABLE -->
         <div>
             <div class="tableadmin">
-                AQUI VA LA TABLA
+            
+            <table class="records_list table-striped table-bordered table-hover" id="mydatatable">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>Referencia</th>
+                        <th>Nombre</th>
+                        <th>id Tipologia</th>
+                        <th>Precio venta</th>
+                        <th>Precio compra</th>
+                        <th>Cantidad</th>
+                        <th>Habilitado</th>
+                        <th>Fecha</th>
+                        
+                        <th>Descripcion</th>
+                        <th>Genero</th>
+                        <th>Imagen</th>
+                        <th>Emocion</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th>Filter..</th>
+                        <th>Filter..</th>
+                        <th>Filter..</th>
+                        <th>Filter..</th>
+                        <th>Filter..</th>
+                        <th>Filter..</th>
+                        <th>Filter..</th>
+                        <th>Filter..</th>
+                        <th>Filter..</th>
+                        <th>Filter..</th>
+                        <th>Filter..</th>
+                        <th>Filter..</th>
+                        <th>Filter..</th>
+                    </tr>
+                </tfoot>
+                <tbody>
+                <?php
+    $sentencia = $pdo->prepare("SELECT * FROM productos");
+    $sentencia -> execute();
+    $listaproductos=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php foreach($listaproductos as $producto) {  ?>
+                   <tr>
+
+                        <td><?php echo $producto['id'] ?></td>
+                        <td><?php echo $producto['codigo'] ?></td>
+                        <td><?php echo $producto['nombre'] ?></td>
+                        <td><?php echo $producto['tipologia_id'] ?></td>
+                        <td><?php echo $producto['precio_venta'] ?></td>
+                        <td><?php echo $producto['precio_compra'] ?></td>
+                        <td><?php echo $producto['cantidad'] ?></td>
+                        <td><?php echo $producto['habilitado'] ?></td>
+                        <td><?php echo $producto['fecha'] ?></td>
+                        
+                        <td><?php echo $producto['descripcion'] ?></td>
+                        <td><?php echo $producto['genero'] ?></td>
+                        <td><?php echo $producto['imagen'] ?></td>
+                        <td><?php echo $producto['emocion'] ?></td>
+
+
+                    </tr>
+                    <?php } ?>                  
+     
+                </tbody>
+            </table>
+            
+                <link rel="stylesheet" href="../assets/style/mydatatable.css">
+                <script src="../assets/js/mydatatable.js"></script>
             </div>
         </div><!-- FIN DIV PARA EL DATATABLE -->
     </div>
@@ -48,10 +136,6 @@ include "../../global/conexion.php";
 
     <!-- JS, Popper.js, and jQuery -->
 
-    <script src="../assets/librerias/jquery-3.5.1.min.js"></script>
-    <script src="../assets/librerias/popper.min.js"></script>
-    <script src="../assets/librerias/bootstrap.min.js"></script>    
 </body>
 </html>
 
-<?php include "../navbar_footer/scd_footer.php";?>
